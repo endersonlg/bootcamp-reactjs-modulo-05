9ce5b70ee2a813f921d7611a2bd946f4dc760d37
endersonlg <endersonluciog@gmail.com>
2020-03-04 17:01:43 -0300

    12 - Carregando dados da API
    Aula feita ate 5:11 minutos.

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
+import api from '../../services/api';
 
 // import { Container } from './styles';
 
-export default function Repository({ match }) {
-  return <h1>Repository: {decodeURIComponent(match.params.repository)}</h1>;
+export default class Repository extends Component {
+  async componentDidMount() {
+    const { match } = this.props;
+
+    const repoName = decodeURIComponent(match.params.repository);
+
+    const [repository, issues] = await Promise.all([
+      api.get(`/repos/${repoName}`),
+      api.get(`/repos/${repoName}/issues`),
+    ]);
+
+    console.log(repository);
+    console.log(issues);
+  }
+
+  render() {
+    return <h1> Repository</h1>;
+  }
 }
